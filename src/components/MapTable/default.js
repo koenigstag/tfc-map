37b95a754a665f5238cab3944d3227fd16af2386
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-08-05 01:09:41 +0300

    Remove unused default puzzles

--- src/components/MapTable/default.js
+++ src/components/MapTable/default.js
@@ -12,32 +12,8 @@ const defaultPieces = [
     imgSrc: '/map pieces/SotA_Map_Puzzle_Story2.png',
   },
   {
-    filename: 'SotA_Map_Puzzle_Story3.png',
-    imgSrc: '/map pieces/SotA_Map_Puzzle_Story3.png',
-  },
-  {
-    filename: 'SotA_Map_Puzzle_Story4.png',
-    imgSrc: '/map pieces/SotA_Map_Puzzle_Story4.png',
-  },
-  {
-    filename: 'SotA_Map_Puzzle_Story5.png',
-    imgSrc: '/map pieces/SotA_Map_Puzzle_Story5.png',
-  },
-  {
     filename: 'SotA_Map_Puzzle_Story6.png',
     imgSrc: '/map pieces/SotA_Map_Puzzle_Story6.png',
-  },
-  {
-    filename: 'SotA_Map_Puzzle_Story8.png',
-    imgSrc: '/map pieces/SotA_Map_Puzzle_Story8.png',
-  },
-  {
-    filename: 'SotA_Map_Puzzle_Story9.png',
-    imgSrc: '/map pieces/SotA_Map_Puzzle_Story9.png',
-  },
-  {
-    filename: 'SotA_Map_Puzzle_Story10.png',
-    imgSrc: '/map pieces/SotA_Map_Puzzle_Story10.png',
   },
   {
     filename: 'SotA_Map_Puzzle_Story7a.png',
